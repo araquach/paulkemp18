5b26735911dacba703bcb57b94288695cd2e1f44
vagrant <vagrant@homestead>
2019-01-30 19:01:23 +0000

    Fixed Louise to Sarah

--- resources/views/pages/leaver.blade.php
+++ resources/views/pages/leaver.blade.php
@@ -19,13 +19,13 @@
 	<section id="leavercopy">
 	    
         <h2>Kate is no longer at PK</h2>
-        <p><strong>We're sorry to inform you that <em>Kate</em> is no longer at PK. She has decided the time had come to try something new. She's been part of the team for many years so it's sad to see her go but we're sure she'll be happy in her new role.</strong></p>
+        <p><strong>We're sorry to inform you that <em>Kate</em> is no longer at PK. She decided the time had come to try something new. She's been part of the team for many years so it's sad to see her go but we're sure she'll be happy in her new role.</strong></p>
         <p>If you were a regular client of Kate we apologise for her departure and we hope it won't cause you any inconvenience.</p>
         
         <p>As you know, we have a number of talented stylists that will be able to look after you! <em>Our new additions to the team could be a perfect choice!</em></p> 
         <p><strong>Brad</strong> joined us at the back end of the year and is at the same level as Kate and he's been producing some outstanding work! You've probably seen him in the salon. <a href="https://paulkemphairdressing.com/blog/introducing_brad" target="_blank">Click here to find out more about him</a></p>
 
-        <p><strong>Louise</strong> joined the team at the beginning of the year and has instantly settled in. She's got a huge amount of experience and is eager to build her client base. She's a Senior Stylist so her prices are a little more expensive than Kate's but you can guarantee she'll deliver! <a href="https://paulkemphairdressing.com/blog/introducing_sarah" target="_blank">Click here to find out more about her</a></p>
+        <p><strong>Sarah</strong> joined the team at the beginning of the year and has instantly settled in. She's got a huge amount of experience and is eager to build her client base. She's a Senior Stylist so her prices are a little more expensive than Kate's but you can guarantee she'll deliver! <a href="https://paulkemphairdressing.com/blog/introducing_sarah" target="_blank">Click here to find out more about her</a></p>
 
         <p>We have all the other familiar faces in the salon who will happily look after you too. Just keep in mind that they are all seriously busy and you might struggle to get in with them. Feel free to take a look at our team page to see if there's a stylist that you'd like to try. <a href="https://paulkemphairdressing.com/team" target="_blank">Check out the full team here</a></p>
         
@@ -40,7 +40,7 @@
         
         <p><a href="{{ URL::to('booking') }}"" target="_blank">Book in with one of our stylists here ></a></p>
         
-        <p><em>Pictured: Brad &amp; Louise</em></p>
+        <p><em>Pictured: Brad &amp; Sarah</em></p>
     
     </section> <!--end leavercopy-->
     
