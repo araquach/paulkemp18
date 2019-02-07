3126c63d89789b93b34e2267764500375c5e352c
vagrant <vagrant@homestead>
2019-02-07 14:34:11 +0000

    Added how to redeem offer

--- resources/views/pages/offers/brad.blade.php
+++ resources/views/pages/offers/brad.blade.php
@@ -30,7 +30,8 @@
 	<div class="ind-offer-copy">
 		<p><strong>Come in for a FREE consultation and if you like the ideas Brad comes up with then book in to get 20% off your visit!</strong></p>
 		<p><strong>Call 01925 444488 to book</strong><br>or use our online booking (link at top of page)</p>
-		<p>Just mention the offer at the time of your visit<br>(Skin Test reqyured 48 hrs before any colour service for new clients)</p>
+		<p>Just mention the offer at the time of your visit</p>
+		<p class="small">Skin Test required 48 hrs before any colour service for new clients)</p>
 		<p class="link"><a href="https://paulkemphairdressing.com/blog/introducing_brad" target="_blank">Click here to find out more about him &gt;</a></p>
 	</div>
 
