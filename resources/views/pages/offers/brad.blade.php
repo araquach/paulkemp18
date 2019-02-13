01d31c4456335e74851a1b022feb990a14612dc5
vagrant <vagrant@homestead>
2019-02-13 11:13:55 +0000

    Some minor changes to the offer pages

--- resources/views/pages/offers/brad.blade.php
+++ resources/views/pages/offers/brad.blade.php
@@ -28,7 +28,7 @@
 	<div id="ind-offer-img-brad"></div>
 
 	<div class="ind-offer-copy">
-		<p><strong>Come in for a FREE consultation and if you like the ideas Brad comes up with then book in to get 20% off your visit!</strong></p>
+		<p><strong>Come in for a FREE consultation and if you like the ideas Brad comes up with then book in to get 20% off your visit with him!</strong></p>
 		<p><strong>Call 01925 444488 to book</strong><br>or use our online booking (link at top of page)</p>
 		<p>Just mention the offer at the time of your visit</p>
 		<p class="small">(Skin Test required 48 hrs before any colour service for new clients)</p>
