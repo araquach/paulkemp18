f5227afc364153ecbee68049227b03a9831634f8
vagrant <vagrant@homestead>
2019-02-17 14:23:32 +0000

    started work on the home pah=ge Re-sass

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<html class="no-js">
+<html>
 
 @section('head')
 
@@ -7,94 +7,67 @@
 
 <body>
 
-	<div class="container" id="page">
+	<div class="container">
 
-		<header class>
-			<div id="logo" class="logo"></div>
-			<h1>Paul Kemp Hairdressing</h1>
-			<h2>Hairdressers in Warrington</h2>
+		<header>
 
-			@if(Request::is('prospect/*'))
-			<nav class="group">
-				<ul>
-					<li>{!! link_to('prospect/taster', 'Taster Package applicants') !!}</li>
-					<li>{!! link_to('prospect/freeproducts', 'Free Product applicants') !!}</li>
-					<li>{!! link_to('/logout', 'Logout') !!}</li>
-				</ul>
-			</nav>
-			
-			@else
-			
-			<nav>
-				<ul>
-					<li>{!! link_to('/', 'Home') !!}</li>
-					<!--<li>{!! link_to('offers', 'Offers') !!}</li>-->
-					<li>{!! link_to('salon', 'The Salon') !!}</li>
-					<li>{!! link_to('team', 'Team') !!}</li>
-					<li>{!! link_to('blog', 'Blog') !!}</li>
-					<li>{!! link_to('recruitment', 'Recruitment') !!}</li>
-					<li>{!! link_to('men', 'Men') !!}</li>
-					<li>{!! link_to('prices', 'Prices') !!}</li>
-					<li>{!! link_to('contact', 'Contact') !!}</li>
-				</ul>
-			</nav>
-			
-			@endif
-			
-			@if(!Request::is('booking'))
-				{!! link_to('booking', 'Book Now', ['class' => 'book-now']) !!}
-			@endif
+			<section class="hero is-primary">
+			  <div class="hero-body">
+			    <div class="container">
+			      <h1 class="title">
+			        Paul Kemp Hairdressing
+			      </h1>
+			      <h2 class="subtitle">
+			        Hairdressers in Warrington
+			      </h2>
+			    </div>
+			    <nav class="nav">					
+					<ul>
+						<li class="button is-link">{!! link_to('/', 'Home') !!}</li>
+						<!--<li>{!! link_to('offers', 'Offers') !!}</li>-->
+						<li class="button is-link">{!! link_to('salon', 'The Salon') !!}</li>
+						<li class="button is-link">{!! link_to('team', 'Team') !!}</li>
+						<li class="button is-link">{!! link_to('blog', 'Blog') !!}</li>
+						<li class="button is-link">{!! link_to('recruitment', 'Recruitment') !!}</li>
+						<li class="button is-link">{!! link_to('men', 'Men') !!}</li>
+						<li class="button is-link">{!! link_to('prices', 'Prices') !!}</li>
+						<li class="button is-link">{!! link_to('contact', 'Contact') !!}</li>
+					</ul>
+					@if(!Request::is('booking'))
+						{!! link_to('booking', 'Book Now', ['class' => 'book-now']) !!}
+					@endif
+				</nav>
+			  </div>
+			</section>
 			
 		</header>
 
-		<div id="content">
+		<div>
 			@yield('content')
 		</div>
 
-		<footer class="clear_fix">
+		<footer class="footer">
 		
-			<ul id="footer_images">
-				<li><a href="http://www.schwarzkopf-professional.com/" target="_blank">{{ Html::image('images/footer/schwarzkopf.png', 'Schwarzkopf - The Hairdressers choice') }}</a></li>
-				<li><a href="http://www.tigihaircare.com/consumer/en-UK/bedhead/" target="_blank">{{ Html::image('images/footer/bedhead.png', 'Bedhead - Proffessional Hairdressing') }}</a></li>
-				<li><a href="http://www.catwalkbytigi.com" target="_blank">{{ Html::image('images/footer/catwalk.png', 'Tigi Catwalk') }}</a></li>
-				<li><a href="http://www.tigihaircare.com/consumer/en-UK/b4men/default.asp" target="_blank">{{ Html::image('images/footer/bformen.png', 'B for Men - mens products for hairdressers') }}</a></li>
-				<li><a href="http://www.ghdhair.com/" target="_blank">{{ Html::image('images/footer/ghd.png', 'ghd - for hairdressers') }}</a></li>
-				<li><a href="http://www.tigihaircare.com/consumer/en-UK/sfactor/default.asp" target="_blank">{{ Html::image('images/footer/sfactor.png', 'S Factor - quality brand for hairdressers') }}</a></li>
-			</ul>
-			
-			<section id="smallabout">
-				<h2><strong>Paul Kemp Hairdressing</strong></h2>
-				<ul>
-				<li>Paul Kemp Warrington</li>
-				<li>Hairdressers Warrington</li> 
-				<li>Hair Salon Warrington</li> 
-				<li>Salons Warrington</li>
-				<li>Hairdressing Warrington</li>
-				<li>Hairdressers Cheshire</li> 
-				<li>Salons Cheshire</li>
-				<li>Hair Salon Cheshire</li> 
-				<li>Hairdressing Cheshire</li>
-				<li>Hair cutting Warrington</li> 
-				<li>Hair colouring Warrington</li>
-				<li>Mens Hairdressing Warrington</li>
-				<li>Mens Hairdressers Warrington</li> 
-				<li>Technical hairdressing</li> 
-				<li>Kebelo Warrington</li> 
-				<li>Brazilian Blowdry Warrington</li> 
-				<li>Opti-Smooth Warrington</li> 
-				<li>Fashion styling Warrington</li> 
-				<li>Mens cutting Warrington</li> 
-				<li>Schwarzkopf Warrington</li> 
-				<li>Tigi Warrington</li> 
-				<li>Bedhead Warrington</li>
-				<li>Osis Warrington</li>
-				<li>Aveda Warrington</li>
-				<li>Catwalk Warrington</li>
-				<li>Bonacure Warrington</li>
-				<li>Warrington Hairdressers</li>
-				<li>Cheshire Hairdressers</li>
-				</ul>
-			</section>
+			<div class="columns">
+				<div class="column">
+					<a href="http://www.schwarzkopf-professional.com/" target="_blank">{{ Html::image('images/footer/schwarzkopf.png', 'Schwarzkopf - The Hairdressers choice') }}</a>
+				</div>
+				<div class="column">
+					<a href="http://www.tigihaircare.com/consumer/en-UK/bedhead/" target="_blank">{{ Html::image('images/footer/bedhead.png', 'Bedhead - Proffessional Hairdressing') }}</a>
+				</div>
+				<div class="column">
+					<a href="http://www.catwalkbytigi.com" target="_blank">{{ Html::image('images/footer/catwalk.png', 'Tigi Catwalk') }}</a>
+				</div>
+				<div class="column">
+					<a href="http://www.tigihaircare.com/consumer/en-UK/b4men/default.asp" target="_blank">{{ Html::image('images/footer/bformen.png', 'B for Men - mens products for hairdressers') }}</a>
+				</div>
+				<div class="column">
+					<a href="http://www.ghdhair.com/" target="_blank">{{ Html::image('images/footer/ghd.png', 'ghd - for hairdressers') }}</a></li>
+				</div>
+				<div class="column">
+					<a href="http://www.tigihaircare.com/consumer/en-UK/sfactor/default.asp" target="_blank">{{ Html::image('images/footer/sfactor.png', 'S Factor - quality brand for hairdressers') }}</a>
+				</div>
+			</div>
 
 			<div id="social">
 				<a href="https://www.facebook.com/PaulKempHairdressing" target="_blank"><div class="social_logo facebook"></div></a>
