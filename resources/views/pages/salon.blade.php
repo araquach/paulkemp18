f0c805d5fbeca8e342a78fe61e1515c2922bbc7c
Araquach <adam@contra-design.com>
2019-02-19 20:17:56 +0000

    Update salon.blade.php

--- resources/views/pages/salon.blade.php
+++ resources/views/pages/salon.blade.php
@@ -23,7 +23,7 @@
     
     <div class="section tile is-ancestor">
       <div class="tile is-parent is-5">
-        <div class="tile is-child box">
+        <div class="tile is-child">
             <h2 class="title 2 has-text-primary">The Salon</h2>
             <p>With a prime position right on the corner of <em>Sankey Street</em> and <em>Springfield street</em> the salon premises is an early 1900's building that was once home to the Picturedrome and Cameo cinemas! It's taken on many guises since, but when we got hold of it we knew we wanted it to be the warmest, friendliest most relaxing salon in the area.</p>
             <p>The salon was designed to create the ultimate in relaxing environments. With cosy, defined areas, you never feel too exposed, but its still open enough to soak up the vibrant, sociable atmosphere. The warm, friendly staff are always on hand to make you feel at home. Enjoy a cappuccino, latte or a glass of wine whilst you are being pampered.</p>
@@ -32,25 +32,37 @@
         </div>
       </div>
       <div class="tile is-parent is-vertical">
-        <div class="tile is-child box">
-            Pic 1
+        <div class="tile is-child">
+            <figure class="image is-240x160">
+                <img class="image" src="{{ asset('images/salon/salon_shot_1.jpg') }}">
+            </figure>
         </div>
-        <div class="tile is-child box">
-            Pic 2
+        <div class="tile is-child">
+            <figure class="image is-240x160">
+                <img class="image" src="{{ asset('images/salon/salon_shot_2.jpg') }}">
+            </figure>
         </div>
-        <div class="tile is-child box">
-            Pic 3
+        <div class="tile is-child">
+            <figure class="image is-240x160">
+                <img class="image" src="{{ asset('images/salon/salon_shot_3.jpg') }}">
+            </figure>
         </div>
       </div>
       <div class="tile is-parent is-vertical">
-        <div class="tile is-child box">
-            Pic 4
+        <div class="tile is-child">
+            <figure class="image is-240x160">
+                <img class="image" src="{{ asset('images/salon/salon_shot_4.jpg') }}">
+            </figure>
         </div>
-        <div class="tile is-child box">
-            Pic 5
+        <div class="tile is-child">
+            <figure class="image is-240x160">
+                <img class="image" src="{{ asset('images/salon/salon_shot_5.jpg') }}">
+            </figure>
         </div>
-        <div class="tile is-child box">
-            Pic 6
+        <div class="tile is-child">
+            <figure class="image is-240x160">
+                <img class="image" src="{{ asset('images/salon/salon_shot_6.jpg') }}">
+            </figure>
         </div>
 
       </div>
