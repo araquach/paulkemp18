efe4b601bb92fef32c3aaae20597f346b1f61a2d
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-03-02 15:24:04 +0000

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,27 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/assets/css/fonts/fonts.css',
+    'resources/assets/css/normalize.css',
+    'resources/assets/css/pkstyles.css',
+    'resources/assets/css/form.css'
+], 'public/css/app.css');
+
+mix.styles([
+    'resources/assets/css/mobile.css'
+], 'public/css/mob.css');
+
+mix.scripts([
+    'resources/assets/js/jquery.js',
+    'resources/assets/js/feedback-feed.js',
+    'resources/assets/js/feedback-feed-ind.js',
+    'resources/assets/js/mobile-nav.js'
+], 'public/scripts/jquery.js');
+
+mix.scripts([
+    'resources/assets/js/mootools-core.js',
+    'resources/assets/js/mootools-more.js'
+], 'public/scripts/mootools.js');
+
+
