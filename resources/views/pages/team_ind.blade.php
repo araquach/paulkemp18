a97c23c3449cd1ce2bdff92f8824be05b6a1c86b
Araquach <adam@contra-design.com>
2019-03-06 15:51:19 +0000

    styled the team individual pages

--- resources/views/pages/team_ind.blade.php
+++ resources/views/pages/team_ind.blade.php
@@ -23,6 +23,13 @@
 			<figure class="image">
 				{{ Html::image('images/staff/' . $team_member->class . '.jpg', $team_member->name) }}
 			</figure>
+			<div id="feedback_feed_ind" class="box is-shadowless">
+			  	<ul>
+			  		@foreach($reviews as $review)
+			      	    <li class="is-size-5">&quot;{{ $review->review }}&quot;<br><span class="is-size-7">{{ remove_client($review->client) }} - hair by {{ remove_staff($review->staff) }} </span></li>
+			      	@endforeach
+			  	</ul>
+			</div>
 		</div>
 		<div class="column section">
 			<h2 class="title is-3 has-text-primary">{{ $team_member->name }}</h2>
@@ -30,23 +37,20 @@
 			<p>{{ $team_member->para2 }}</p>
 			<p>{{ $team_member->para3 }}</p>
 			<p class="has-text-weight-bold is-size-5 is-bold">Average Cut &amp; Colour price with {{ $team_member->review_link }} : &pound;{{ $team_member->price }}*</p>
-			<section>
-    			<p class="has-text-centered is-size-7">*average prices for half-head foils or equivalent plus a Cut Dry and Style - before any discounts</p>
-			</section>
-			<div id="feedback_feed_ind">
-			  	<ul>
-			  		@foreach($reviews as $review)
-			      	    <li>&quot;{{ $review->review }}&quot;<br><span class="client">{{ remove_client($review->client) }} - hair by {{ remove_staff($review->staff) }} </span></li>
-			      	@endforeach
-			  	</ul>
-			  	@if($reviews->count() > 0)
-					<a href="{{ URL::to('reviews/' . $team_member->class ) }}">More of {{ $team_member->review_link }}'s reviews here &gt;</a>
+			<div class="team-ind-bottom">
+				@if($reviews->count() > 0)
+					<a href="{{ URL::to('reviews/' . $team_member->class ) }}" class="button is-primary">More of {{ $team_member->review_link }}'s reviews here</a>
 				@endif
-			</div>
-			<div id="fb-like">
-				<div class="fb-like" data-href="http://www.paulkemphairdressing.com/{{ $team_member->class }}" data-width="250" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+				<br><br>
+				<div id="fb-like">
+					<div class="fb-like" data-href="http://www.paulkemphairdressing.com/{{ $team_member->class }}" data-width="250" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+				</div>
+				<section>
+	    			<p class="has-text-centered is-size-7">*average prices for half-head foils or equivalent plus a Cut Dry and Style - before any discounts</p>
+				</section>
 			</div>
 		</div>
+			
 	</div>
 </div>
 
