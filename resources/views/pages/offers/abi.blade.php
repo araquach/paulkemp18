4413968495b04e782e426bade55f4ac53f37c15e
Araquach <adam@contra-design.com>
2019-03-06 14:42:42 +0000

    Styled the Abi Page

--- resources/views/pages/offers/abi.blade.php
+++ resources/views/pages/offers/abi.blade.php
@@ -17,28 +17,38 @@
 
 @include('layouts.partials.fb_like')
 
-<section id="ind-offer">
-
-	<div class="ind-offer-copy">
-		<h1>Special introductory offer with our recently promoted stylist Abi</h1>
-		<p>Abi is flying up the ranks and the quality of her work is second to none! Check out some of her recent looks in the pics!</p>
-		<p><strong>If you're considering a new stylist then take advantage of this great intro offer with her&hellip;</strong></p>
+<div id="ind-offer">
+	<div class="columns">
+		<div class="column is-8">
+			<div class="top section">
+				<h2 class="title is-2 is-size-4-mobile has-text-primary">Special introductory offer with our recently promoted stylist Abi</h2>
+				<p class="is-size-5">Abi is flying up the ranks and the quality of her work is second to none! Check out some of her recent looks in the pics!</p>
+				<p class="is-size-4"><strong>If you're considering a new stylist then take advantage of this great intro offer with her&hellip;</strong></p>
+			</div>
+		</div>
 	</div>
+	
+	<figure class="image">
+		{{ Html::image('/images/offer/individual/abi_montage_wide.png') }}
+	</figure>
 
-	<div id="ind-offer-img-abi"></div>
+	<div class="columns">
+		<div class="column is-8">
+			<div class="section">
+				<p class="is-size-5"><strong>Come in for a FREE consultation and if you like the ideas Abi comes up with then book in to get 20% off your visit!</strong></p>
+				<p class="is-size-4"><strong>Call 01925 444488 to book</strong><br>
+				or use our online booking (link at top of page)</p>
+				<p>Just mention the offer at the time of your visit</p>
+				<p class="small">(Skin Test required 48 hrs before any colour service for new clients)</p>
+				<a href="{{ URL::to('team/abi') }}" target="_blank" class="button is-primary">Find out more about Abi</a>
+				<br><br>
 
-	<div class="ind-offer-copy">
-		<p><strong>Come in for a FREE consultation and if you like the ideas Abi comes up with then book in to get 20% off your visit!</strong></p>
-		<p><strong>Call 01925 444488 to book</strong><br>or use our online booking (link at top of page)</p>
-		<p>Just mention the offer at the time of your visit</p>
-		<p class="small">(Skin Test required 48 hrs before any colour service for new clients)</p>
-		<p class="link"><a href="{{ URL::to('team/abi') }}" target="_blank">Click here to find out more about her &gt;</a></p>
+				<div id="fb-like">
+				<div class="fb-like" data-href="{{ URL::to('offer/abi') }}" data-width="250" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
+				</div>
+			</div>
+		</div>
 	</div>
-
-	<div id="fb-like">
-		<div class="fb-like" data-href="{{ URL::to('offer/abi') }}" data-width="250" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
-	</div>
-
-</section> <!--end #offers-->
+</div>
 
 @stop
