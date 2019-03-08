f1e1aa46253df6a8b92936ee84a277a8b4664400
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-03-08 12:50:54 +0000

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,7 +17,7 @@
   </div>
    <div class="columns">
     <div class="column is-5 is-offset-7">
-      <section class="section is-size-4-mobile">
+      <section class="section is-size-5-mobile">
         <h2 class="title 2 has-text-primary has-text-centered">A New Standard of Hairdressing</h2>
         <p>Paul Kemp Hairdressing is located right in the heart of Warrington town centre. Sister salon to the award winning <em><a href="http://www.jakatasalon.co.uk/" target="_blank">Jakata</a></em>, the stunning salon opened in June 2011 with the aim to offer an ultra relaxing atmosphere, first class customer service, alongside the highest level of hairdressing expertise.</p>
         <p>The talented hairdressing team are all trained to the highest level in cutting, colouring and styling hair, with specialists in technical colour, hair straightening, wedding hair and hair extensions. The team has a wealth of experience in all aspects of hairdressing so you can just sit back, relax and enjoy the experience.</p>
