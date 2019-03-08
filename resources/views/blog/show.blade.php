aec3fdbbd589672187129d0aded129c711e57794
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-03-08 12:58:39 +0000

    Update show.blade.php

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -16,7 +16,7 @@
 
 @include('layouts.partials.fb_like')
 
-<div id="blog" class="section box">
+<div id="blog" class="section box has-text-5-mobile">
     <h2 class="title is-2">{{ $blog->title }}</h2> 
     <article class="columns">
         <div class="column is-5">
