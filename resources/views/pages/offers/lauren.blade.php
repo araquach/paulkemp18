db34438196edd298682dc166e6802fbd01274d73
Araquach <adam@contra-design.com>
2019-03-13 12:49:15 +0000

    Sorted the images

--- resources/views/pages/offers/lauren.blade.php
+++ resources/views/pages/offers/lauren.blade.php
@@ -17,28 +17,40 @@
 
 @include('layouts.partials.fb_like')
 
-<section id="ind-offer">
+<div id="ind-offer">
+	<div class="columns">
+		<div class="column is-8">
+			<div class="top section">
+				<h2 class="title is-2 is-size-4-mobile has-text-primary">
+				Apprentice Lauren is Taking on More clients</h2>
+				<h3 class="subtitle is-3">A Colour &amp; Cut from just &pound;20!</h3>
+				<p class="is-size-5">At Paul Kemp Hairdressing we train and develop our stylists to the highest standards. Lauren has reached the point in her apprenticeship where she needs loads of clients!</p>
+				<p class="is-size-4"><strong>She's progressing at an amazing rate and is now looking to get as much experience as possible.</strong></p>
+			</div>
+		</div>
 
-	<div class="ind-offer-copy">
-		<h1>Special introductory offer with our latest Addition to the team</h1>
-		<p>Our latest <strong>Senior Stylist</strong> has over 12 years experience and is a seriously skilled hairdresser!</p>
-		<p>If you're considering a new stylist then take advantage of this great intro offer with her&hellip;</p>
 	</div>
+	
+	<figure class="image">
+		{{ Html::image('/images/offer/individual/lauren_montage_wide.png') }}
+	</figure>
 
-	<div id="ind-offer-img-sarah"></div>
+	<div class="columns">
+		<div class="column is-8">
+			<div class="section">
+				<p class="is-size-5"><strong>Come in for a FREE consultation and if you like the ideas Lauren comes up with then get booked in with her!</strong></p>
+				<p class="is-size-4"><strong>Call 01925 444488 to book</strong><br>
+				or use our online booking (link at top of page)</p>
+				<p class="small">(Skin Test required 48 hrs before any colour service for new clients)</p>
+				<a href="{{ URL::to('team/lauren') }}" target="_blank" class="button is-primary">Find out more about Lauren</a>
+				<br><br>
 
-	<div class="ind-offer-copy">
-		<p><strong>Come in for a FREE consultation and if you like the ideas Sarah comes up with then book in to get 20% off your visit!</strong></p>
-		<p><strong>Call 01925 444488 to book</strong><br>or use our online booking (link at top of page)</p>
-		<p>Just mention the offer at the time of your visit</p>
-		<p class="small">(Skin Test required 48 hrs before any colour service for new clients)</p>
-		<p class="link"><a href="{{ URL::to('team/lauren') }}" target="_blank">Click here to find out more about her &gt;</a></p>
+				<div id="fb-like">
+				<div class="fb-like" data-href="{{ URL::to('offer/lauren') }}" data-width="250" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
+				</div>
+			</div>
+		</div>
 	</div>
-
-	<div id="fb-like">
-		<div class="fb-like" data-href="{{ URL::to('offer/lauren') }}" data-width="250" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
-	</div>
-
-</section> <!--end #offers-->
+</div>
 
 @stop
