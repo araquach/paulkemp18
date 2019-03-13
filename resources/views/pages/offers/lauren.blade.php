4449d6995b0bf30fbba0a7a57f898e5bd4d8a2a4
Araquach <adam@contra-design.com>
2019-03-13 13:26:27 +0000

    Update lauren.blade.php

--- resources/views/pages/offers/lauren.blade.php
+++ resources/views/pages/offers/lauren.blade.php
@@ -5,7 +5,7 @@
 @include('layouts.partials.head', [
 	'description' => 'Lauren is looking to build her client base. She\'s close to qualifying and wants to gain more experience',
 	'keywords' => 'hair offers, hairdressing offers, salon offers, Paul Kemp Hairdressing offers',
-	'ogtitle' => 'Book in with Lauren',
+	'ogtitle' => 'Apprentice Lauren is Taking on More clients',
 	'ogdescription' => 'Looking for a new stylist? Lauren is close to completing her apprentiship and is looking to build her client base. Book in with her - you won\'t be dissapointed!',
 	'ogimage' => url('/') . '/images/offer/individual/lauren_montage_fb.jpg',
 	'title' => 'Paul Kemp Hairdressing - Offer with Lauren - Hairdressers in Warrington'
