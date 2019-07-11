146f6575fc5dfa46dae2105a9ec54be95af24d39
Araquach <adam@contra-design.com>
2019-07-11 17:31:55 +0100

    Update reviews.blade.php

--- resources/views/pages/reviews.blade.php
+++ resources/views/pages/reviews.blade.php
@@ -10,7 +10,7 @@
 	'ogimage' => url('/') . '/images/reviews/reviews-meta.jpg',
 	'title' => 'Paul Kemp Hairdressing - Reviews - Hairdressers in Warrington'
 	])
-	
+
 @stop
 
 @section('content')
@@ -19,12 +19,12 @@
 	<div class="columns section">
 		<div class="column is-8">
 			<h2 class="title is-3 has-text-primary">Some of our recent client reviews</h2>
-			
+
 			<h3 class="subtitle is-5 has-text-primary">Filter by stylist</h3>
-			
+
 			<div class="columns is-multiline is-mobile">
 				@foreach($team_members as $team_member)
-					<div class="column is-2-desktop is-2-tablet is-3-mobile">
+					<div class="column is-3-desktop is-2-tablet is-4-mobile">
 						<a href="{{ URL::to('reviews/' . $team_member->class) }}">
 							<div class="card">
 								<div class="card-image">
