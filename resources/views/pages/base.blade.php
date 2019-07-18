3420fa1cad73ab3a13bd79450796d6c593d8b75e
Araquach <adam@contra-design.com>
2019-07-18 15:39:49 +0100

    Create base.blade.php

---
+++ resources/views/pages/base.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('head')
+
+@include('layouts.partials.head', [
+'title' => 'Paul Kemp Hairdressing - Abi Moves to Base - Hairdressers in Warrington'
+])
+
+@stop
+
+@section('content')
+
+<div id="base">
+  <h1 class="title is-2">Abi is moving to Base Hairdressing</h1>
+</div>
+
+@stop
