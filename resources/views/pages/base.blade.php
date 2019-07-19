a115e2371298f1f4413f1da7e8822fd99799ba2a
Adam Carter <adam@contra-design.com>
2019-07-19 07:58:41 +0100

    Update base.blade.php

--- resources/views/pages/base.blade.php
+++ resources/views/pages/base.blade.php
@@ -11,7 +11,17 @@
 @section('content')
 
 <div id="base">
-  <h1 class="title is-2">Abi is moving to Base Hairdressing</h1>
+  <h1 class="title is-2">Abi is relocating to our new salon</h1>
+
+  <p>We our opening a brand new salon called Base Hairdressing that will ultimately be an academy to train up the next generation of PK and Jakata stylists.</p>
+  <p>The new salon is at our previous Jakata site which we've spent the last 18 moths renovating - it looks amazing!</p>
+  <p>To get things off the ground we're taking a selection of our existing stylists from both sites to help launch the brand.</p>
+  <p>Abi has volunteered and will be joining Adam, Jimmy and Lauren from Jakata. She will be working from Base hairdressing for the next 6 to 12 months. We will also be recruiting apprentices and also renting out a number of chairs too.</p>
+  <p>If you are a client of Abi's, your future appointments will be at Base Hairdressing. All appointments have been transferred and remain the same - you'll just be at our brand new salon.</p>
+  <p>I hope you're as excited as us about the move, we're confident the transition will be smooth and you'll love the new salon! (Though there might be a couple of teething problems along the way! Hopefully not too many!)</p>
+  <p>If you have any questions regarding the changes, don't hesitate to get in touch by calling PK on 01925 444488</p>
+  <p>Our new phone number for Base Hairdressing is 01925 444449 and will be in use from Tuesday 23rd July</p>
+  <p>Abi and the rest of the team look forward to seeing you in the new salon!</p>
 </div>
 
 @stop
