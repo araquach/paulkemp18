868697ca66627da56d86b3a5c26012f23d0b0ffb
Araquach <adam@contra-design.com>
2019-07-19 15:40:12 +0100

    Got the Base Abi article created

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -35,68 +35,68 @@ class PagesController extends Controller {
 	public function index()
 	{
 		$reviews = Review::where('salon', '2')->orderByRaw("RAND()")->get();
-		
+
 		$blogs = Blog::take(4)->where('publish', 2)->orWhere('publish', 3)
 			->with('paras')->orderBy('created_at', 'desc')->get();
-	
+
 		return view('pages.home', compact('reviews', 'blogs'));
 	}
-	
+
 	public function details()
 	{
 		return view('pages.details');
 	}
-	
+
 	public function news()
 	{
 		return view('pages.news');
 	}
-	
+
 	public function oldnews()
 	{
 		return view('pages.oldnews');
 	}
-	
+
 	public function offers()
 	{
 		return view('pages.offers');
 	}
-	
+
 	public function men()
 	{
 		return view('pages.men');
 	}
-	
+
 	public function prices()
 	{
 		return view('pages.prices_stylist');
 	}
-	
+
 	public function apprenticePrices()
 	{
 		return view('pages.prices_apprentice');
 	}
-	
+
 	public function recruitment()
 	{
 		return view('pages.recruitment');
 	}
-	
+
 	public function salon()
 	{
 		return view('pages.salon');
 	}
-	
+
 	public function booking()
 	{
 		return view('pages.booking');
 	}
-	
+
 	public function kebelo()
 	{
 		return view('pages.kebelo');
-	}	
-	
+	}
+
 	public function team()
 	{
 		$team_members = TeamMember::where('salon', 2)->orderBy('position')->get();
@@ -109,22 +109,22 @@ class PagesController extends Controller {
 		$team_member = TeamMember::where('salon', 2)->where('class', $team )->first();
 
 		$reviews = Review::where('salon', 2)->where('staff', 'LIKE', '%' . $team . '%')->orderByRaw("RAND()")->get();
-		
+
 		return view('pages.team_ind', compact('team_member', 'reviews'));
 
 		dd($team_member->name);
 	}
-	
+
 	public function newstaff()
 	{
 		return view('pages.new_staff');
 	}
-	
+
 	public function leaver()
 	{
 		return view('pages.leaver');
 	}
-	
+
 	public function reviews($stylist = 'all')
 	{
 		$team_members = TeamMember::where('salon', 2)->orderBy('position')->get();
@@ -140,7 +140,7 @@ class PagesController extends Controller {
 			->where('staff', 'LIKE', '%'.$stylist.'%')
 			->orderByRaw("RAND()")->limit(9)->get();
 		}
-		
+
 		return view('pages.reviews', compact('reviews', 'team_members'));
 	}
 
@@ -162,6 +162,11 @@ class PagesController extends Controller {
 	public function offerLauren()
 	{
 		return view('pages.offers.lauren');
+	}
+
+	public function base()
+	{
+		return view('pages.base');
 	}
 
 	public function bulma()
