8d3cf20697a713ce9c5b25d5de1844071ec386af
Adam Carter <adam@contra-design.com>
2019-08-22 18:14:27 +0100

    Update PotentialEmployeeController.php

--- app/Http/Controllers/PotentialEmployeeController.php
+++ app/Http/Controllers/PotentialEmployeeController.php
@@ -14,33 +14,33 @@ class PotentialEmployeeController extends Controller
     public function __construct(PotentialEmployee $potential)
 	{
         $this->middleware('auth', ['except' => ['create', 'store']]);
-		
+
 		$this->potential = $potential;
 	}
 
-    
+
     public function create()
     {
         return view('potential_employee.create');
     }
 
-    
+
     public function store(PotentialEmployeeFormRequest $request)
     {
         $input = $request->all();
-        
+
         PotentialEmployee::create($input);
-        
+
         $applicant = PotentialEmployee::where('salon', 2)->get()->last();
-		
+
 		Mail::send('emails.potential_employee', compact('applicant'), function($message)
    		{
        		$message->from('booking@jakatasalon.co.uk', 'Jakata');
-			$message->to('adam@jakatasalon.co.uk');
-       		// $message->to('jimmy@jakatasalon.co.uk');
-       		$message->subject('New Potential Employee');
+			    $message->to('adam@jakatasalon.co.uk');
+       		$message->to('jimmy@jakatasalon.co.uk');
+       		$message->subject('New Potential Employee (PK)');
    		});
-    
+
         return redirect()->back()->with('message', 'Thanks! We\'ll be in touch soon');
     }
 }
