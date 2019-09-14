1bd49031a9d195978600d31ef23f396f37566e13
Adam Carter <adam@contra-design.com>
2019-09-14 16:03:06 +0100

    minor adjustments

--- resources/views/pages/base.blade.php
+++ resources/views/pages/base.blade.php
@@ -52,118 +52,123 @@
 			<p>All of our <strong>apprentices</strong> and <strong>graduate stylists</strong> are now located at Base where we can offer our full selection of hair services at a fraction of the cost. All services are fully supervised by Senior Stylists and all our usual high quality products are used.</p>
 
 			<h2 class="title is 2 has-text-primary">The Base Team</h2>
-
-			<div class="columns">
-				<div class="column is-4">
-					<figure class="image">
-						<img src="/images/base/abi.jpg" alt="Abi">
-					</figure>
+			<div id="base-team">
+				<div class="columns">
+					<div class="column is-4">
+						<figure class="image">
+							<img src="/images/base/abi.jpg" alt="Abi">
+						</figure>
+					</div>
+					<div class="column">
+						<h2 class="title is-4 has-text-primary">Abi</h2>
+						<h3 class="subtitle has-text-primary">Advanced Graduate</h3>
+						<p><strong>Abi is an extremely progressive hairdresser. She recently won a place on the GHD Style Squad - a team of just 8 stylists from all over the UK, hand picked by GHD.</strong></p>
+						<p>She produces some beautiful cuts, colours and styles. Her friendly, approachable personality ensures she always gets 5 star feedback from her clients.</p>
+						<small>Abi has temporarily joined us from Paul Kemp Hairdressing to help get Base up and running.</small>
+						<small>Average Colour &amp; Cut: &pound;80</small>
+					</div>
 				</div>
-				<div class="column">
-					<h2 class="title is-4 has-text-primary">Abi</h2>
-					<h3 class="subtitle">Advanced Graduate</h3>
-					<p><strong>Abi is an extremely progressive hairdresser. She recently won a place on the GHD Style Squad - a team of just 8 stylists from all over the UK, hand picked by GHD.</strong></p>
-					<p>She produces some beautiful cuts, colours and styles. Her friendly, approachable personality ensures she always gets 5 star feedback from her clients.</p>
-					<small>Abi has temporarily joined us from Paul Kemp Hairdressing to help get Base up and running.</small>
-					<small>Average Colour &amp; Cut: &pound;80</small>
+				<div class="columns">
+					<div class="column is-4">
+						<figure class="image">
+							<img src="/images/base/lauren_t.jpg" alt="Lauren T">
+						</figure>
+					</div>
+					<div class="column">
+						<h2 class="title is-4 has-text-primary">Lauren T</h2>
+						<h3 class="subtitle has-text-primary">Graduate</h3>
+						<p><strong>Lauren is a Graduate Stylist with loads of flair and a huge passion for the industry. She loves creating bold, statement looks, particularly short cuts and funky styles.</strong></p>
+						<p>She loves clients that are looking for something a bit different and open to ideas - if you're wanting a new look then book in for a consultation with her!</p>
+						<small>Average Colour &amp; Cut: &pound;60</small>
+					</div>
 				</div>
-			</div>
-			<div class="columns">
-				<div class="column is-4">
-					<figure class="image">
-						<img src="/images/base/lauren_t.jpg" alt="Lauren T">
-					</figure>
+				<div class="columns">
+					<div class="column is-4">
+						<figure class="image">
+							<img src="/images/base/david.jpg" alt="David">
+						</figure>
+					</div>
+					<div class="column">
+						<h2 class="title is-4 has-text-primary">David</h2>
+						<h3 class="subtitle has-text-primary">Graduate</h3>
+						<p><strong>David is our latest recruit. He completed his training at Andrew Collinge Graduates in Liverpool and is now looking to work his way to stylist level.</strong></p>
+						<p>He produces beautiful colour and cutting work and his blow-dry's are stunning!</p>
+						<small>Average Colour &amp; Cut: &pound;60</small>
+					</div>
 				</div>
-				<div class="column">
-					<h2 class="title is-4 has-text-primary">Lauren T</h2>
-					<h3 class="subtitle">Graduate</h3>
-					<p><strong>Lauren is a Graduate Stylist with loads of flair and a huge passion for the industry. She loves creating bold, statement looks, particularly short cuts and funky styles.</strong></p>
-					<p>She loves clients that are looking for something a bit different and open to ideas - if you're wanting a new look then book in for a consultation with her!</p>
-					<small>Average Colour &amp; Cut: &pound;60</small>
+				<div class="columns">
+					<div class="column is-4">
+						<figure class="image">
+							<img src="http://via.placeholder.com/200x220" alt="Lauren W">
+						</figure>
+					</div>
+					<div class="column">
+						<h2 class="title is-4 has-text-primary">Lauren W</h2>
+						<h3 class="subtitle has-text-primary">Junior Stylist</h3>
+						<p><strong>Lauren's skills are progressing fast and is eager to make the move up to Graduate level. She's producing some great colours and cuts and is eager to try her skills out on more clients.</strong></p>
+						<p>She loves all aspects of hairdressing so get booked in with her before her prices go up!</p>
+						<small>Average Colur &amp; Cut: &pound;35</small>
+					</div>
 				</div>
-			</div>
-			<div class="columns">
-				<div class="column is-4">
-					<figure class="image">
-						<img src="/images/base/david.jpg" alt="David">
-					</figure>
-				</div>
-				<div class="column">
-					<h2 class="title is-4 has-text-primary">David</h2>
-					<h3 class="subtitle">Graduate</h3>
-					<p><strong>David is our latest recruit. He completed his training at Andrew Collinge Graduates in Liverpool and is now looking to work his way to stylist level.</strong></p>
-					<p>He produces beautiful colour and cutting work and his blow-dry's are stunning!</p>
-					<small>Average Colour &amp; Cut: &pound;60</small>
-				</div>
-			</div>
-			<div class="columns">
-				<div class="column is-4">
-					<figure class="image">
-						<img src="http://via.placeholder.com/200x220" alt="Lauren W">
-					</figure>
-				</div>
-				<div class="column">
-					<h2 class="title is-4 has-text-primary">Lauren W</h2>
-					<h3 class="subtitle">Junior Stylist</h3>
-					<p><strong>Lauren's skills are progressing fast and is eager to make the move up to Graduate level. She's producing some great colours and cuts and is eager to try her skills out on more clients.</strong></p>
-					<p>She loves all aspects of hairdressing so get booked in with her before her prices go up!</p>
-					<small>Average Colur &amp; Cut: &pound;35</small>
-				</div>
-			</div>
-			<div class="columns">
-				<div class="column is-4">
-					<figure class="image">
-						<img src="http://via.placeholder.com/200x220" alt="Lucy">
-					</figure>
-				</div>
-				<div class="column">
-					<h2 class="title is-4 has-text-primary">Lucy</h2>
-					<h3 class="subtitle">Junior Stylist</h3>
-					<p><strong>Lucy is in the second year of her apprenticeship and is already producing some amazing work.</strong></p>
-					<p>She has recently opened her column up and is looking to gain experience in all aspects of hairdressing.</p>
-					<p>Get booked in with her to see what she can do for you!</p>
-					<small>Average Colur &amp; Cut: &pound;35</small>
+				<div class="columns">
+					<div class="column is-4">
+						<figure class="image">
+							<img src="http://via.placeholder.com/200x220" alt="Lucy">
+						</figure>
+					</div>
+					<div class="column">
+						<h2 class="title is-4 has-text-primary">Lucy</h2>
+						<h3 class="subtitle has-text-primary">Junior Stylist</h3>
+						<p><strong>Lucy is in the second year of her apprenticeship and is already producing some amazing work.</strong></p>
+						<p>She has recently opened her column up and is looking to gain experience in all aspects of hairdressing.</p>
+						<p>Get booked in with her to see what she can do for you!</p>
+						<small>Average Colur &amp; Cut: &pound;35</small>
+					</div>
 				</div>
 			</div>
 
 			<h2 class="title is-3 has-text-primary">Prices</h2>
 
 			<table class="table">
-				<tr>
-					<th></th>
-					<th>Advanced<br>Graduate</th>
-					<th>Graduate</th>
-					<th>Junior Stylist</th>
-					<th>Junior</th>
-				</tr>
-				<tr>
-					<th>Cut, Dry & Style</th>
-					<td>&pound;30</td>
-					<td>&pound;25</td>
-					<td>&pound;15</td>
-					<td>&pound;10</td>
-				</tr>
-				<tr>
-					<th>Colour</th>
-					<td>&pound;40</td>
-					<td>&pound;35</td>
-					<td>&pound;15</td>
-					<td>&pound;10</td>
-				</tr>
-				<tr>
-					<th>Blow Dry</th>
-					<td>&pound;15</td>
-					<td>&pound;15</td>
-					<td>&pound;10</td>
-					<td>&pound;5</td>
-				</tr>
-				<tr>
-					<th>Men's Cut &amp Style</th>
-					<td>&pound;20</td>
-					<td>&pound;15</td>
-					<td>&pound;10</td>
-					<td>&pound;5</td>
-				</tr>
+				<thead>
+					<tr>
+						<th></th>
+						<th class="is-size-7-mobile">Advanced<br>Graduate</th>
+						<th class="is-size-7-mobile">Graduate</th>
+						<th class="is-size-7-mobile">Junior Stylist</th>
+						<th class="is-size-7-mobile">Junior</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th class="is-size-7-mobile">Cut, Dry & Style</th>
+						<td>&pound;30</td>
+						<td>&pound;25</td>
+						<td>&pound;15</td>
+						<td>&pound;10</td>
+					</tr>
+					<tr>
+						<th class="is-size-7-mobile">Colour</th>
+						<td>&pound;40</td>
+						<td>&pound;35</td>
+						<td>&pound;15</td>
+						<td>&pound;10</td>
+					</tr>
+					<tr>
+						<th class="is-size-7-mobile">Blow Dry</th>
+						<td>&pound;15</td>
+						<td>&pound;15</td>
+						<td>&pound;10</td>
+						<td>&pound;5</td>
+					</tr>
+					<tr>
+						<th class="is-size-7-mobile">Men's Cut &amp Style</th>
+						<td>&pound;20</td>
+						<td>&pound;15</td>
+						<td>&pound;10</td>
+						<td>&pound;5</td>
+					</tr>
+				</tbody>
 			</table>
 			<p>All prices are a guide. A specific price will be quoted on consultation. A skin test is required 48 hours before any colour service for new clients</p>
 
