312633f09a3aa29cd4d110a757eee5d94bd93350
Adam Carter <adam@contra-design.com>
2019-09-17 14:50:46 +0100

    Got it working

--- resources/views/hairmodel/create.blade.php
+++ resources/views/hairmodel/create.blade.php
@@ -10,19 +10,18 @@
 
 @section('content')
 
-<div id="hairmodel">
-
-    <div id="hairmodel-copy">
-
+<div id="hair-models">
+    <div class="section columns">
+      <div class="column is-8 is-size-5-mobile">
 			@if(Session::has('message'))
-        <div class="success">
+        <div class="box is-size-4">
           {{{ Session::get('message') }}}
         </div>
       @endif
 
-      <h1>We need models!</h1>
+      <h1 class="title is-3 has-text-primary">We need models!</h1>
 
-      <p>We're looking for models for our training days! Get your hair done for practically free! (sometimes a nominal charge to cover product costs applies)</p>
+      <p><strong>We're looking for models for our training days! Get your hair done for practically free! (sometimes a nominal charge to cover product costs applies)</strong></p>
   		<p>If you'd like to volunteer simply add your name to our model database and we'll contact you when we have a session that fits your needs</p>
       <p>Please provide some info about your hair to allow us to determine which sessions best suit your needs</p>
       <ul>
@@ -33,51 +32,52 @@
       </ul>
       <small>A skin test is required 48 hours before we can colour your hair if you haven't been to us before. We will not be able to carry out any colour treatments if we don't have a record of this.</small>
 
-      <div class="form">
+      <div>
         {!! Form::open(array(
-        	'action' => 'HairModelController@store', 'id' => 'form'
+        	'action' => 'HairModelController@store', 'class' => 'form'
         )) !!}
 
 				{!! Form::open(array(
 					'action' => 'ProspectController@store', 'id' => 'prospect-form'
 				)) !!}
 
-        <p class="note">Fields with <span class="required">*</span> are required.</p>
+        <p>Fields with <span class="has-text-danger">*</span> are required.</p>
 
         @if (count($errors) > 0)
-          <div id="hairmodel-form_es_" class="errorSummary">
+          <div class="box has-text-danger">
           		<p>Please fix the following input errors:</p>
           		<ul>
           	   		 @foreach($errors->all() as $error)
-          	        <li>{{{ $error }}}</li>
+          	        <li class="has-text-danger">{{{ $error }}}</li>
           	    	@endforeach
           		</ul>
           </div>
         @endif
 
-        <div class="row">
-          {!! Form::label('name', 'Full Name') !!}
-          {!! Form::text('name') !!}
-          {!! $errors->first('name', '<div class="errorMessage">:message</div>') !!}
+        <div class="field">
+          {!! Form::label('name', 'Full Name',  ['class' => 'label required']) !!}
+          {!! Form::text('name', '', ['class' => 'input is-info']) !!}
+          {!! $errors->first('name', '<div class="has-text-danger">:message</div>') !!}
         </div>
 
-        <div class="row">
-          {!! Form::label('mobile', 'Mobile Number') !!}
-          {!! Form::text('mobile') !!}
-          {!! $errors->first('mobile', '<div class="errorMessage">:message</div>') !!}
+        <div class="field">
+          {!! Form::label('mobile', 'Mobile Number', ['class' => 'label required']) !!}
+          {!! Form::text('mobile', '', ['class' => 'input is-info']) !!}
+          {!! $errors->first('mobile', '<div class="has-text-danger">:message</div>') !!}
         </div>
 
-        <div class="row">
-          {!! Form::label('hairinfo', 'Additional Information') !!}
-          {!! Form::textarea('hairinfo') !!}
-          {!! $errors->first('hairinfo', '<div class="errorMessage">:message</div>') !!}
+        <div class="field">
+          {!! Form::label('hairinfo', 'Additional Information', ['class' => 'label required']) !!}
+          {!! Form::textarea('hairinfo', '', ['class' => 'textarea is-info']) !!}
+          {!! $errors->first('hairinfo', '<div class="has-text-danger">:message</div>') !!}
         </div>
 
-        <div class="row buttons">
-      	   {!! Form::submit('Send') !!}
+        <div>
+      	   {!! Form::submit('Send', ["class" => "button"]) !!}
         </div>
 
         {!! Form::close() !!}
+        </div>
       </div>
     </div>
 </div>
