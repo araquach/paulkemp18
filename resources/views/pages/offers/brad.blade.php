5cb0939be7788032d4f1cf3bb525d2c1ae829997
Adam Carter <adam@contra-design.com>
2019-09-21 12:35:09 +0100

    Update dthe offers

--- resources/views/pages/offers/brad.blade.php
+++ resources/views/pages/offers/brad.blade.php
@@ -41,7 +41,7 @@
 				<p class="is-size-4"><strong>Call 01925 444488 to book</strong><br>
 				or use our online booking (link at top of page)</p>
 				<p>Just mention the offer at the time of your visit</p>
-				<p class="small">(Offer ends 27/09/19 - Skin Test required 48 hrs before any colour service for new clients)</p>
+				<p class="small">(Offer ends 31/10/19 - Skin Test required 48 hrs before any colour service for new clients)</p>
 				<a href="{{ URL::to('team/brad') }}" target="_blank" class="button is-primary">Find out more about Brad</a>
 				<br><br>
 
