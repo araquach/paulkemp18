f021b44774dda787eae15b781c3bf644d04815fb
Adam Carter <adam@contra-design.com>
2020-02-15 12:42:47 +0000

    Added new David page

--- resources/views/pages/offers/david.blade.php
+++ resources/views/pages/offers/david.blade.php
@@ -7,10 +7,10 @@
 	'keywords' => 'hair offers, hairdressing offers, salon offers, Paul Kemp Hairdressing offers',
 	'ogtitle' => 'A special offer with our latest addition to the team - David',
 	'ogdescription' => 'Looking for a new stylist? David is our latest skilled stylist to join the team',
-	'ogimage' => url('/') . '/images/offer/individual/brad_montage_fb.jpg',
+	'ogimage' => url('/') . '/images/offer/individual/david_montage_fb.jpg',
 	'ogimagewidth' => '1200',
 	'ogimageheight' => '628',
-	'title' => 'Paul Kemp Hairdressing - Offer with Brad - Hairdressers in Warrington'
+	'title' => 'Paul Kemp Hairdressing - Offer with David - Hairdressers in Warrington'
 	])
 
 @stop
@@ -23,8 +23,9 @@
 	<div class="columns">
 		<div class="column is-8">
 			<div class="top section">
-				<h2 class="title is-2 is-size-4-mobile has-text-primary">Special offer with Brad</h2>
-				<p class="is-size-5">Brad has loads of experience and is a seriously skilled hairdresser! Check out his work in the pics!</p>
+				<h2 class="title is-2 is-size-4-mobile has-text-primary">Special introductory offer with David</h2>
+				<p class="is-size-5">David recentyly joined the team after completing his training at the Andrew Collinge Graduates Academy in Liverpool.</p>
+				<p>He's fully settled into the PK team and is creating some stunning looks!</p>
 				<p class="is-size-4"><strong>If you're considering a new stylist then take advantage of this great offer with him&hellip;</strong></p>
 			</div>
 		</div>
@@ -37,16 +38,16 @@
 	<div class="columns">
 		<div class="column is-8">
 			<div class="section">
-				<p class="is-size-5"><strong>Come in for a FREE consultation and if you like the ideas Brad comes up with then book in to get 25% off your visit!</strong></p>
+				<p class="is-size-5"><strong>Come in for a FREE consultation and if you like the ideas David comes up with then book in to get a Colour &amp; Cut for just &pound;50</strong></p>
 				<p class="is-size-4"><strong>Call 01925 444488 to book</strong><br>
 				or use our online booking (link at top of page)</p>
 				<p>Just mention the offer at the time of your visit</p>
 				<p class="small">(Offer ends 31/03/20 - Skin Test required 48 hrs before any colour service for new clients)</p>
-				<a href="{{ URL::to('team/brad') }}" target="_blank" class="button is-primary">Find out more about Brad</a>
+				<a href="{{ URL::to('team/david') }}" target="_blank" class="button is-primary">Find out more about David</a>
 				<br><br>
 
 				<div id="fb-like">
-				<div class="fb-like" data-href="{{ URL::to('offer/brad') }}" data-width="250" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
+				<div class="fb-like" data-href="{{ URL::to('offer/david') }}" data-width="250" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
 				</div>
 			</div>
 		</div>
