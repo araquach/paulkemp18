17c16482b68758b9fcea73740cf4669885f772e6
Adam Carter <adam@contra-design.com>
2020-06-04 17:24:34 +0100

    Updated the booking page

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -16,9 +16,7 @@
 
 <section id="corona">
 	<div class="columns">
-		<div class="section column is-7">
-
-		<div class="section contact-form">
+		<div class="column section is-7">
 
 		@if(Session::has('message'))
 			<div class="success">
@@ -28,11 +26,15 @@
 
 			<h1 class="title is-2 has-text-primary">Register for your appointment</h1>	
 
+			<p class="is-size-4">We're getting closer and closer to our reopening date and we can't wait!!</p>
+			<p>We are not taking booking until we have a confirmed date as it's still a bit up in the air at the moment. <strong>Saturday July 4th</strong> is looking the most likely but there is the potential this could be brought forward or pushed back. We're still in the process of creating new schedules for the team to minimise the number of people in the salon as your safety is our priority.</p>
+			<p>Each team member will be contacting their own clients to arrange bookings about a week or two before our opening date. Anybody who was already booked in will be contacted first. We will be working really hard to make sure everyone gets an appointment that suits them in the quickest possible time.</p>
+			<p class="is-size-4">If you are wanting to book an appointment, please fill in the form below to ensure we've got your correct information</p>
 			<p>Please fill in as much information as possible in the form below, we will get back to you as soon as we can</p>
 
 			<ul>
 				@foreach($errors->all() as $error)
-					<li>{{{ $error }}}</li>
+					<li class="has-text-danger">{{{ $error }}}</li>
 				@endforeach
 			</ul>
 
@@ -43,19 +45,19 @@
 				<div class="field">
 				{!! Form::label('first_name', 'First Name', ['class' => 'label']) !!}
 				{!! Form::text('first_name', old('first_name'), ['class' => 'input']) !!}
-				{!! $errors->first('first_name', '<div class="errorMessage">:message</div>') !!}
+				{!! $errors->first('first_name', '<div class="has-text-danger">:message</div>') !!}
 				</div>
 				
 				<div class="field">
 				{!! Form::label('last_name', 'Last Name', ['class' => 'label']) !!}
 				{!! Form::text('last_name', old('last_name'), ['class' => 'input']) !!}
-				{!! $errors->first('last_name', '<div class="errorMessage">:message</div>') !!}
+				{!! $errors->first('last_name', '<div class="has-text-danger">:message</div>') !!}
 				</div>
 				
 				<div class="field">
 				{!! Form::label('mobile', 'Mobile Number', ['class' => 'label']) !!}
 				{!! Form::text('mobile', old('mobile'), ['class' => 'input']) !!}
-				{!! $errors->first('mobile', '<div class="errorMessage">:message</div>') !!}
+				{!! $errors->first('mobile', '<div class="has-text-danger">:message</div>') !!}
 				</div>
 
 				<div class="field">
@@ -69,23 +71,21 @@
 						'Abi'=> 'Abi',
 						'David'=> 'David',
 						'Unknown'=> 'Unknown'
-						), old('stylist'), ['placeholder' => 'Select']) !!}
-					{!! $errors->first('stylist', '<div class="errorMessage">:message</div>') !!}
-					</div>
+						), old('stylist'), ['placeholder' => 'Select Stylist', 'class'=> 'select']) !!}
+					{!! $errors->first('stylist', '<div class="has-text-danger">:message</div>') !!}
+				</div>
 				
 				<div class="field">
 				{!! Form::submit('Submit', ['class' => 'button']) !!}
 				</div>
 			
 			{!! Form::close() !!}
-
-		</div><!-- form -->
 		</div>
 		<div class="column">
             <figure class="image is-hidden-mobile">
                 {{ Html::image('images/corona/corona.jpg') }}   
             </figure>
-        </div>
+		</div>
 	</div>
 </section>
 
