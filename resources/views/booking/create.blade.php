dfb9f9f18686c27311416e72bddca266cda76cf2
Adam Carter <adam@contra-design.com>
2020-06-25 14:57:33 +0100

    Update create.blade.php

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -28,9 +28,9 @@
 
 			<h1 class="title is-2 has-text-primary">Register for your appointment</h1>	
 
-			<p class="is-size-4">We're getting closer and closer to our reopening date and we can't wait!!</p>
-			<p>We are not taking booking until we have a confirmed date as it's still a bit up in the air at the moment. <strong>Saturday July 4th</strong> is looking the most likely but there is the potential this could be brought forward or pushed back. We're still in the process of creating new schedules for the team to minimise the number of people in the salon as your safety is our priority.</p>
+			<p class="is-size-4">It's confirmed - we'll definitely be re-opening on Saturday 4th July!</p>
 			<p>Each team member will be contacting their own clients to arrange bookings about a week or two before our opening date. Anybody who was already booked in will be contacted first. We will be working really hard to make sure everyone gets an appointment that suits them in the quickest possible time.</p>
+			<p>We will not be opening our online bookings or taking telephone bookings until we re-open.</p>
 			<p class="is-size-4">If you are wanting to book an appointment, please fill in the form below to ensure we've got your correct information</p>
 			<p>Please fill in as much information as possible in the form below, we will get back to you as soon as we can</p>
 
